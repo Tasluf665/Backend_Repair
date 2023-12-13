0b7166d7d31a0d99b158baa163d327b0705b23da
Md. Tasluf Morshed <78264660+Tasluf665@users.noreply.github.com>
2023-12-13 22:14:43 +0600

    SendEmail API address correction 2

--- utils/SendEmail.js
+++ utils/SendEmail.js
@@ -54,7 +54,7 @@ const sendVerificationEmail = async (email, _id) => {
   let BackEndURL = config.get("URL");
   BackEndURL = BackEndURL.endsWith("/") ? BackEndURL : BackEndURL + "/";
   const message = `<h2>Please click on the given link to activate your account. This link will expire in 20 minutes</h2>
-  <p>${BackEndURL}/api/users/authentication/${token}</p>
+  <p>${BackEndURL}api/users/authentication/${token}</p>
   `;
 
   // Send the email with the activation token link.
@@ -81,7 +81,7 @@ const sendResetPasswordEmail = async (email, _id) => {
   let BackEndURL = config.get("URL");
   BackEndURL = BackEndURL.endsWith("/") ? BackEndURL : BackEndURL + "/";
   const message = `<h2>Please click on the given link to reset your account password. This link will expire in 20 minutes</h2>
-  <p>${BackEndURL}/api/auth/reset-password/${token}</p>
+  <p>${BackEndURL}api/auth/reset-password/${token}</p>
   `;
 
   // Send the email with the reset token link.
